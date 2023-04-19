e5e73a37cf1882983d9dd7db9e72e1212cf2a42d
Bruno Alexandre <brunoalencaralexandre@gmail.com>
2023-04-19 13:56:09 -0300

    feat: created Divider component

---
+++ src/components/Divider/Divider.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="divider" />
+</template>
+
+<script>
+export default {
+  name: 'Divider'
+}
+</script>
+
+<style scoped>
+.divider {
+  background-color: rgba(196, 196, 196, 0.5);
+  width: 100%;
+  height: 1px;
+  margin: 16px 0;
+}
+</style>
